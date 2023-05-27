bb7b47c2a054ab6d1f762fdc4d2fb6a1ff636c54
pgdeguzman <pgdeguzman@up.edu.ph>
2023-05-27 21:30:33 +0800

    May 27

--- myapp/data.py
+++ myapp/data.py
@@ -5,8 +5,8 @@ db_path = 'pa.db'
 conn = sqlite3.connect(db_path)
 cur = conn.cursor()
 query = 'SELECT * FROM pets WHERE type=?'
-a_type = input("Please enter animal type:")
-results = cur.execute(query, [a_type, '?']).fetchall()
+a_type = input("Please enter animal type: ")
+results = cur.execute(query, [a_type]).fetchall()
 for item in results:
     print(item)
 conn.close()
