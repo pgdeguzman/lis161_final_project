b96b914c6a9bc722cd77eaecd5a9ce4fed69add1
pgdeguzman <pgdeguzman@up.edu.ph>
2023-06-26 09:02:56 +0800

    ...

--- myapp/app.py
+++ myapp/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 import os
-from data import *
+import sqlite3
 from functools import wraps
 
 app = Flask(__name__)
@@ -9,6 +9,91 @@ app.secret_key = 'cosmo_apparel_secret_key'
 
 picFolder = os.path.join('static', 'img')
 app.config['UPLOAD_FOLDER'] = picFolder
+
+db_path = "items.db"
+
+def connect_to_db(path):
+    conn = sqlite3.connect(path)
+    conn.row_factory = sqlite3.Row
+    return conn, conn.cursor()
+
+def get_item_by_name(name):
+    conn, cur = connect_to_db(db_path)
+    query = 'SELECT *, "/static/img/" || image AS image_url FROM items WHERE name = ?'
+    value = name
+    result = cur.execute(query, (value,)).fetchone()
+    conn.close()
+    return result
+
+def get_all_items():
+    conn, cur = connect_to_db(db_path)
+    query = 'SELECT *, "/static/img/" || image AS image_url FROM items'
+    results = cur.execute(query).fetchall()
+    conn.close()
+    return results
+
+def insert_item(product_data):
+    conn, cur = connect_to_db(db_path)
+    query = 'INSERT INTO items (name, price, size, image, description, stock, timestamp) VALUES (?, ?, ?, ?, ?, ?, ?)'
+    values = (
+        product_data['name'],
+        product_data['price'],
+        product_data['size'],
+        product_data['image'],
+        product_data['description'],
+        product_data['stock'],
+        product_data['timestamp']
+    )
+    cur.execute(query, values)
+    conn.commit()
+    conn.close()
+
+def update_item(product_data):
+    conn, cur = connect_to_db(db_path)
+    query = "UPDATE items SET name=?, price=?, size=?, image=?, description=? WHERE id=?"
+    values = (
+        product_data['name'],
+        product_data['price'],
+        product_data['size'],
+        product_data['image'],
+        product_data['description'],
+        product_data['product_id'],
+    )
+    cur.execute(query, values)
+    conn.commit()
+    conn.close()
+
+def delete_item(product_data):
+    conn, cur = connect_to_db(db_path)
+    query = "DELETE FROM items WHERE id=?"
+    values = (product_data['product_id'],)
+    cur.execute(query, values)
+    conn.commit()
+    conn.close()
+
+def update_item_stock(item_id, new_stock):
+    conn, cur = connect_to_db(db_path)
+    query = "UPDATE items SET stock=? WHERE id=?"
+    values = (new_stock, item_id)
+    cur.execute(query, values)
+    conn.commit()
+    conn.close()
+
+def update_stock(name, stock):
+    conn, cur = connect_to_db(db_path)
+    query = "UPDATE items SET stock = ? WHERE name = ?"
+    values = (stock, name)
+    cur.execute(query, values)
+    conn.commit()
+    conn.close()
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if not session.get('logged_in'):
+            return redirect(url_for('login'))
+        return f(*args, **kwargs)
+    return decorated_function
 
 @app.route('/')
 @app.route('/home')
@@ -44,14 +129,6 @@ def login():
     else:
         return render_template('login.html')
 
-def login_required(f):
-    @wraps(f)
-    def decorated_function(*args, **kwargs):
-        if not session.get('logged_in'):
-            return redirect(url_for('login'))
-        return f(*args, **kwargs)
-    return decorated_function
-
 @app.route('/logout')
 def logout():
     session.clear()
@@ -84,14 +161,10 @@ def processing():
     product_data = {
         "name": request.form['p_name'],
         "price": request.form['p_price'],
-        "sizes": {
-            request.form['p_size']: request.form['p_price']
-        },
+        "size": request.form['p_size'],
         "image": request.form['p_url'],
         "description": request.form['p_desc'],
-        "stock": {
-            request.form['p_size']: 10  # Assuming initial stock is 10 for all sizes
-        },
+        "stock": request.form['p_stock'],
         "timestamp": "Some timestamp"  # Replace with the actual timestamp
     }
     insert_item(product_data)
@@ -102,11 +175,11 @@ def processing():
 def modify():
     if request.form["modify"] == "edit":
         product_id = request.form["product_id"] 
-        item = get_item_by_id(product_id)
+        item = get_item_by_name(product_id)
         return render_template('update.html', item=item)
     elif request.form["modify"] == "delete":
         product_id = request.form["product_id"]
-        item = get_item_by_id(product_id)
+        item = get_item_by_name(product_id)
         delete_item({'product_id': product_id})
         return redirect(url_for('products'))
 
@@ -117,14 +190,12 @@ def update():
         "product_id": request.form['product_id'],
         "name": request.form['p_name'],
         "price": request.form['p_price'],
-        "sizes": {
-            request.form['p_size']: request.form['p_price']
-        },
+        "size": request.form['p_size'],
         "image": request.form['p_url'],
         "description": request.form['p_desc'],
     }
     update_item(product_data)
-    return redirect(url_for('item', product_id=request.form['product_id']))
+    return redirect(url_for('item', name=request.form['p_name']))
 
 @app.route('/buy', methods=['GET', 'POST'])
 def buy():
